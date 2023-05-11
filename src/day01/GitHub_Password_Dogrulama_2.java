995076530a03bad73df1959e1c8397fd6b9d647d
yavuzselimpasa <yavuzselimpasa.2019@gmail.com>
2023-05-11 20:33:37 +0300

    2023.05.11

--- src/day01/GitHub_Password_Dogrulama_2.java
+++ src/day01/GitHub_Password_Dogrulama_2.java
@@ -1,4 +1,19 @@
 package day01;
 
 public class GitHub_Password_Dogrulama_2 {
+
+    /*
+
+                 2. Adım: Uzak URL'yi değiştirin
+Artık kişisel erişim belirtecimizi oluşturduğumuza göre, aşağıdaki komutu çalıştırmamız gerekiyor:
+
+   git uzak set-url kaynağı https://<githubtoken>@github.com/<kullanıcı adı>/<depo adı>.git
+
+   Yukarıdaki komutta değiştirdiğinizden emin olun:
+
+  <githubtoken>önceki adımda kopyaladığınız kişisel erişim belirteci ile
+  <username>GitHub kullanıcı adınız ile
+  <repositoryname>GitHub deponuzun adıyla
+
+     */
 }
